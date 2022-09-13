de97ded157534f61a000ba9337f25640c91767b7
Fabio Galimberti <fabio.glimb@gmail.com>
2022-09-13 17:40:48 +0200

    fix 'deprecated-react-native-prop-types'

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ import {
     Animated,
     Dimensions,
     Text,
-    TouchableWithoutFeedback,
-    ViewPropTypes as RNViewPropTypes,
+    TouchableWithoutFeedback
 } from 'react-native'
+
+import { ViewPropTypes as RNViewPropTypes } from 'deprecated-react-native-prop-types'
 
 import PropTypes from 'prop-types';
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
